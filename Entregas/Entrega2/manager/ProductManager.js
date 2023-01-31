a6d977bca1d1ae563e16cc43f23bbc2958654d27
Mora Ramos <mora25ramos@gmail.com>
2023-01-31 12:21:08 -0300

    Correcciones del desafio

--- Entregas/Entrega2/manager/ProductManager.js
+++ Entregas/Entrega2/manager/ProductManager.js
@@ -17,18 +17,29 @@ export default class ManagerProductos{
         catch (error) {
             console.log (error)
         }
-    }
+    } 
 
     addProduct = async(producto) =>{
         const productos = await this.consultarProductos();
         if (productos.length===0){
             producto.id = 1
         } else {
-            producto.id = productos(productos.length -1).id + 1;
+            producto.id = productos[productos.length -1].id + 1;
        }
+
        productos.push(producto)
 
        await fs.promises.writeFile (path, JSON.stringify(productos, null, '\t'));
        return producto;
+    }
+
+    consultarId = async ()=>{
+        try {
+            const idProducto = await document.getElementById(producto.id)
+            console.log(`El ID del producto es: $ {consultarId}`)
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
 }
